a834541418bafaa87907e26ca39800b34cf5e303
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-29 14:06:03 -0600

    Add sad path for user creates games

--- spec/features/users/user/user_can_create_a_game_spec.rb
+++ spec/features/users/user/user_can_create_a_game_spec.rb
@@ -28,4 +28,22 @@ describe 'A registered user' do
     expect(game.difficulty).to eq("medium")
     expect(game.number_of_questions).to eq("2")
   end
+
+  it 'can create a new game, sad path' do
+    user = User.create!(username: "JoshSherwood1", email: "email@email.com", google_token: "yadayada")
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit "/games/new"
+    name = "Nerdiest Trivia Game Ever!"
+
+    expect(page).to have_content("Create a Game!")
+    select "2", from: :number_of_questions
+    select "Geography", from: :category
+    select "Medium", from: :difficulty
+    click_button "Create Game"
+
+    expect(current_path).to eq("/games/new")
+    expect(page).to have_content("Custom name can't be blank")
+  end
 end
