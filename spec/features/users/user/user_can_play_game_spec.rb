a8264d25aae2f07c73596b8e5b3b96e830c81e0e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-29 15:54:34 -0600

    Update feature spec test for user can play a game with correct css and testing for ending page
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton 
    <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>
    
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- spec/features/users/user/user_can_play_game_spec.rb
+++ spec/features/users/user/user_can_play_game_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 describe 'A registered user' do
-  it 'can play the games_path' do
+  it 'can play the games_path and end up on game end page' do
+    json_response = File.open('./spec/fixtures/trivia_data.json')
+    stub_request(:get, 'https://tomato-time-api.herokuapp.com/api/v1/questions?amount=5&category=Geography&difficulty=easy').to_return(status: 200, body: json_response)
+
     user = User.create!(username: "User Name", email: "email@email.com", google_token: "token")
     game_1 = Game.create!(custom_name: "GeoTime", category: "Geography", number_of_questions: "5", difficulty: "easy", user_id: user.id)
 
@@ -13,8 +16,9 @@ describe 'A registered user' do
 
     #cannot get capybara to select option button
     within '#myModal-0' do
-
-      select('.question_424_false')
+      choose('question_424_true')
+      # select('.question_424_false')
+      # choose(option: 'Russia', allow_label_click: true)
       # choose("Russia", allow_label_click: true)
       # click_on ("Russia")
       # find(:css, '.question_424_false').click
@@ -22,22 +26,36 @@ describe 'A registered user' do
     end
 
     within '#myModal-1' do
+      choose('question_429_true')
       click_button('Next')
     end
 
     within '#myModal-2' do
+      choose('question_430_true')
       # click_button('.option-1')
       click_button('Next')
     end
 
     within '#myModal-3' do
+      choose('question_435_true')
       # click_button('.option-1')
       click_button('Next')
     end
 
     within '#myModal-4' do
+      choose('question_436_true')
       # click_button('.option-1')
       click_button('Submit')
+    end
+
+    expect(current_path).to eq("/games/#{game_1.id}/end")
+    expect(page).to have_content("You got 5 correct answers out of 5!!! Good job!")
+    within ".games_info" do
+      expect(page).to have_content(game_1.custom_name)
+      expect(page).to have_content(game_1.category)
+      expect(page).to have_content(game_1.difficulty)
+      expect(page).to have_button("Restart Game")
+      expect(page).to have_button("Go Back to My Games")
     end
   end
 end
