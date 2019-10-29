197a7620c29e7b91aaeb302cd3bc2c483b13f52c
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-29 09:18:15 -0600

    Removed save_and_open_page

--- spec/features/users/user/user_can_see_game_contents_spec.rb
+++ spec/features/users/user/user_can_see_game_contents_spec.rb
@@ -1,32 +1,45 @@
 require 'rails_helper'
 
 describe 'A registered user' do
-  it 'can see game contents on clicking start button' do
+  it 'can play the games_path' do
     user = User.create!(username: "User Name", email: "email@email.com", google_token: "token")
-    game_1 = Game.create!(custom_name: "BookTime", category: "Entertainment: Books", number_of_questions: "10", difficulty: "easy", user_id: user.id)
+    game_1 = Game.create!(custom_name: "BookTime", category: "Entertainment: Books", number_of_questions: "5", difficulty: "easy", user_id: user.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    visit "/games"
 
-    within ".games" do
-      within "#game-#{game_1.id}" do
-        click_link("Start")
-      end
-    end
-
-    expect(current_path).to eq("/games/#{game_1.id}")
+    visit "/games/#{game.id}"
 
     expect(page).to have_button('Play Game')
 
     within '#myModal-0' do
-      expect(page).to have_content('Question')
-      expect(page).to have_css('#question-0')
-      expect(page).to have_css('.option-1')
-      expect(page).to have_css('.option-2')
-      expect(page).to have_css('.option-3')
-      expect(page).to have_css('.option-4')
+      click_button('.option-1')
+      click_button('Next')
     end
 
-    save_and_open_page
+    within '#myModal-1' do
+      click_button('.option-1')
+      click_button('Next')
+    end
+
+    within '#myModal-2' do
+      click_button('.option-1')
+      click_button('Next')
+    end
+
+    within '#myModal-3' do
+      click_button('.option-1')
+      click_button('Next')
+    end
+
+    within '#myModal-4' do
+      click_button('.option-1')
+      click_button('Next')
+    end
+
+    within '#myModal-5' do
+      click_button('.option-1')
+      click_button('Submit')
+    end
+    
   end
 end
