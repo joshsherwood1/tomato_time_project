1fb6dc6e92f355cc6a790e8554516666cf20a8eb
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-29 09:08:41 -0600

    Create facade for game with questions objects

---
+++ app/facades/games_facade.rb
@@ -0,0 +1,21 @@
+class GamesFacade
+  def initialize(current_user, game_id)
+    @game = current_user.games.find(game_id)
+  end
+
+  def questions
+    get_questions.map do |question|
+      Question.new(question)
+    end
+  end
+
+  def get_questions
+    TomatoTimeApiService.get_data(category: @game.category,
+                                  difficulty: @game.difficulty,
+                                  amount: @game.number_of_questions)
+  end
+
+  def number_of_questions
+    @game.number_of_questions.to_i
+  end
+end
---
+++ app/models/question.rb
@@ -0,0 +1,12 @@
+class Question
+
+  attr_reader :id, :category, :difficulty, :question, :correct_answer, :options
+  def initialize(question)
+    @id = question[:id]
+    @category = question[:geography]
+    @difficulty = question[:difficulty]
+    @question = question[:question]
+    @correct_answer = question[:correct_answer]
+    @options = question[:options]
+  end
+end
