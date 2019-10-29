53ec0f21042575a42fdf0d4be82cc9598ed6ecfd
nancylee713 <srlee713@gmail.com>
2019-10-29 14:42:28 -0600

    Add config setting for mailers to work

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,18 @@ module TomatoTime2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # ActionMailer
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      domain:               'https://peaceful-chamber-62417.herokuapp.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
   end
 end
