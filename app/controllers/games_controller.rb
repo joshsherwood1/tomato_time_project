89bf2ff878a527de084b9390e747dff89ab1cc25
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-29 01:15:09 -0600

    Update calculate_score method to also obtain total number of questions answered
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton 
    <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -28,14 +28,14 @@ class GamesController < ApplicationController
   def show
     # game spec
 
-    game = current_user.games.find(params[:id])
+    @game = current_user.games.find(params[:id])
 
     # create a new game_score object
-    scoreboard = current_user.game_score.create(game: game, user: current_user)
+    scoreboard = current_user.game_score.create(game: @game, user: current_user)
 
     # consume TomatoTime API
-    @questions = TomatoTimeApiService.get_data(category: game.category, difficulty: game.difficulty, amount: game.number_of_questions)
-    @number_of_questions = game.number_of_questions.to_i
+    @questions = TomatoTimeApiService.get_data(category: @game.category, difficulty: @game.difficulty, amount: @game.number_of_questions)
+    @number_of_questions = @game.number_of_questions.to_i
 
     # render game show page
     render locals: {
@@ -46,8 +46,10 @@ class GamesController < ApplicationController
 
   def calculate_score
     number_correct = params["question"].values.count {|value| value == "true"}
-    flash[:success] = "You got #{number_correct} correct answers!!! Good job!"
-    redirect_to games_path
+    total_questions = params["question"].values.count
+    game_score = GameScore.new(user_id: current_user.id, game_id: params["game_id"].to_i, score: number_correct)
+    flash[:success] = "You got #{number_correct} correct answers out of #{total_questions}!!! Good job!"
+    redirect_to "/games/#{params["game_id"].to_i}/stats"
   end
 
   private
