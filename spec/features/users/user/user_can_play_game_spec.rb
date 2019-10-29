7f9918b415e8ea19d8f9c5f84863d18396288c01
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-29 13:38:43 -0600

    Trying to click radio button in modal

--- spec/features/users/user/user_can_play_game_spec.rb
+++ spec/features/users/user/user_can_play_game_spec.rb
@@ -3,43 +3,41 @@ require 'rails_helper'
 describe 'A registered user' do
   it 'can play the games_path' do
     user = User.create!(username: "User Name", email: "email@email.com", google_token: "token")
-    game_1 = Game.create!(custom_name: "BookTime", category: "Entertainment: Books", number_of_questions: "5", difficulty: "easy", user_id: user.id)
+    game_1 = Game.create!(custom_name: "GeoTime", category: "Geography", number_of_questions: "5", difficulty: "easy", user_id: user.id)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit "/games/#{game.id}"
+    visit "/games/#{game_1.id}"
 
-    expect(page).to have_button('Play Game')
+    click_button('Play Game')
 
+    #cannot get capybara to select option button
     within '#myModal-0' do
-      click_button('.option-1')
+
+      :visible => false).click
+      # choose("Russia", allow_label_click: true)
+      # click_on ("Russia")
+      # find(:css, '.question_424_false').click
       click_button('Next')
     end
 
     within '#myModal-1' do
-      click_button('.option-1')
       click_button('Next')
     end
 
     within '#myModal-2' do
-      click_button('.option-1')
+      # click_button('.option-1')
       click_button('Next')
     end
 
     within '#myModal-3' do
-      click_button('.option-1')
+      # click_button('.option-1')
       click_button('Next')
     end
 
     within '#myModal-4' do
-      click_button('.option-1')
-      click_button('Next')
-    end
-
-    within '#myModal-5' do
-      click_button('.option-1')
+      # click_button('.option-1')
       click_button('Submit')
     end
-    
   end
 end
