16782bca90c231279efe5a4ffa8d57aa943e537e
nancylee713 <srlee713@gmail.com>
2019-10-30 12:46:18 -0600

    Add class methods to game_score model
    Co-Authored-By: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-Authored-By: Scott Payton <33855435+scottzero@users.noreply.github.com>

--- app/models/game_score.rb
+++ app/models/game_score.rb
@@ -1,6 +1,24 @@
+require 'date'
+
 class GameScore < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  scope :today, -> { where("created_at >= NOW() - INTERVAL '24 HOURS'") }
+  scope :by_game, -> (game_id) { where(game_id: game_id) }
+
+  def self.average_score_per_game(id)
+    return 0 if self.aggregate_score_per_game(id) == 0
+    (aggregate_score_per_game(id).to_f / aggregate_count_per_game(id)).round(2)
+  end
+
+  def self.aggregate_count_per_game(id)
+    self.by_game(id).count
+  end
+
+  def self.aggregate_score_per_game(id)
+    self.by_game(id).map { |game| game.game_percent_score }.sum
+  end
 
   def game_id
     game.id
