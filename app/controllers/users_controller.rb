60de1eaee3217d0bff0f505355cb5f678e4decb2
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-31 08:15:45 -0600

    Add validation for email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,9 @@ class UsersController < ApplicationController
     if already_user?(friend_email)
       redirect_to invite_path
       flash[:error] = 'This user is already registered on our site.'
+    elsif not_valid_email?(friend_email)
+      flash[:error] = 'Please enter a valid email address.'
+      redirect_to invite_path
     else
       InviteFriendMailer.invite_friend(current_user, friend_email, server_origin).deliver_now
       redirect_to invite_path
@@ -29,5 +32,11 @@ class UsersController < ApplicationController
 
   def already_user?(friend_email)
       User.exists?(email: friend_email)
+  end
+
+  def not_valid_email?(friend_email)
+    if (friend_email =~ URI::MailTo::EMAIL_REGEXP) == nil
+      return true
+    end
   end
 end
